698d85ca3d7eb2887bda1c25f116b30925e3fdcf
Alex Higgins <higgs508@gmail.com>
2021-03-11 21:23:01 -0600

    added files

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,12 @@
-# Display the output
-print("New Python File")
-print("I don't know what I am doing")
+<!doctype html>
+<html>
+	<head>
+		<title>What am I even doing?</title>
+	<script src="script.js"></script>
+	<link rel="stylesheet" href"style.css">
+	</head>
+
+	<body>
+		<h1>Simple, but hard</h1>
+	</body>
+</html>
